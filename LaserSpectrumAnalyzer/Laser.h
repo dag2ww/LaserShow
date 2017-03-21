205e1b73fe585f149694af07e332088c5e0e8ed6
dancewicz.grzegorz <dancewicz.grzegorz@gmail.com>
2017-03-21 18:34:09 +0100

    Adjusted for local hardware

--- LaserSpectrumAnalyzer/Laser.h
+++ LaserSpectrumAnalyzer/Laser.h
@@ -24,11 +24,11 @@
 
 // -- The following flags can be used to rotate/flip the output without changing the DAC wiring, just uncomment the desired swap/flip
 // define this to swap X and Y on the DAC
-#define LASER_SWAP_XY
+//#define LASER_SWAP_XY
 // define this to flip along the x axis
 #define LASER_FLIP_X
 // define this to flip along the y axis
-#define LASER_FLIP_Y
+//#define LASER_FLIP_Y
 
 //! Encapsulates the laser movement and on/off state.
 class Laser
